a3448967b1964b3d915914b28556efeb33300940
giorgi <giorgi.bibilashvili89@gmail.com>
2019-04-28 11:41:23 +0400

    Web QR configuration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { Provider } from "react-redux";
 
-import Posts from "./components/Posts";
-import PostForm from "./components/PostForm";
 import PrivateRoute from "./routes/PrivateRoute";
 
 import store from './store/index';
 
-import index from "./pages/index";
-import about from "./pages/about";
-import posts from "./pages/posts";
-import login from "./pages/login";
+import home from "./pages/home/index";
+// import about from "./pages/about";
+// import posts from "./pages/posts";
+// import login from "./pages/login";
 
 class App extends Component {
   render() {
@@ -22,10 +20,10 @@ class App extends Component {
         <Router>
           <div className="App">
             <Switch>
-              <Route exact path='/' component={index} />
-              <Route exact path='/about' component={about} />
+              <Route exact path='/' component={home} />
+              {/* <Route exact path='/about' component={about} />
               <Route exact path='/login' component={login} />
-              <PrivateRoute exact path='/posts' component={posts} />
+              <PrivateRoute exact path='/posts' component={posts} /> */}
             </Switch>
           </div>
         </Router>
---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'reactstrap';
+
+import './styles.scss';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col xs="auto">
+            ssdasd
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
